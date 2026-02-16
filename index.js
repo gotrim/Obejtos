228ed6c4d1609f29ce2604230c7aade2c0a4d27a
Ar.Ro.Me <147737827+gotrim@users.noreply.github.com>
2026-02-16 06:16:39 +0000

    erro bobo

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ console.log(base);
 
 base.width = 800;
 base.height = 800;
-
-const con = base.gatContex("2d");
+const con = base.getContext("2d");
 console.log(con);
-
 con.fillStyle = "blue";
 con.fillRect(0,0,100,100);
