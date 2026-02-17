4ef6e34c3dbe83d881331f4b4096a35304f08163
Ar.Ro.Me <147737827+gotrim@users.noreply.github.com>
2026-02-17 00:55:05 +0000

    esqueci de piscar

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
+const { createContext } = require("react");
+
 console.log(base);
 
 base.width = 800;
 base.height = 800;
 
+let dz = 1;
+let angulo = 0;
+
 const con = base.getContext("2d");
-const FPS = 60;
+const FPS = 30;
 const dt = 1/FPS;
 const vs = [
-    {x: -0.5, y:  0.5, z: 0.5},
-    {x:  0.5, y: -0.5, z: 0.5},
-    {x:  0.5, y:  0.5, z: 0.5},
-    {x: -0.5, y: -0.5, z: 0.5},
+    {x: -0.25, y:  0.25, z:  0.25},
+    {x:  0.25, y: -0.25, z:  0.25},
+    {x:  0.25, y:  0.25, z:  0.25},
+    {x: -0.25, y: -0.25, z:  0.25},
 
-    {x: -0.5, y:  0.5, z: -0.5},
-    {x:  0.5, y: -0.5, z: -0.5},
-    {x:  0.5, y:  0.5, z: -0.5},
-    {x: -0.5, y: -0.5, z: -0.5},
+    {x: -0.25, y:  0.25, z: -0.25},
+    {x:  0.25, y: -0.25, z: -0.25},
+    {x:  0.25, y:  0.25, z: -0.25},
+    {x: -0.25, y: -0.25, z: -0.25},
 ]
-// _________________________________________________________
-//| seria como sair de dentro de um quadrado para poder ver |
-//|_________________________________________________________|
-let dz = 1;
+
+const fs = [
+    [0, 1, 2, 3],
+    [4, 5, 6, 7],
+]
 
 const fundo = "#1A1A1A";
 const interior = "#8DB600";
@@ -45,6 +51,14 @@ function pont({x, y}){
     //| Altura maxima do espaço                                  |
     //|__________________________________________________________|
     // ( Só pra lembrar oq é cada coisa)
+}
+
+function linha(p1, p2){
+    ctx.strkeStyle = interior;
+    ctx.beginPath()
+    ctx.moveTo(p1.x, p1.y);
+    ctx.lineTo(p2.x, p2.y);
+    ctx.stroke();
 }
 
 function tela(p){
@@ -74,16 +88,46 @@ function progecao({x,y,z}){
 //| mais distantes do centro parecerem menor colocando um (Z) maior  |
 //| se colocar um (Z) como 0 ele mão vai aparecer                    |
 //|__________________________________________________________________|
+
+function rotacao_xz({x, y, z}, angulo){
+    const c = Math.cos(angulo);
+    const s = Math.sin(angulo);
+    return {
+        x : x*c - z*s,
+        y,
+        z : x*s + z*c,
+    };
+// ___________________________________________________
+//| Aqui iremos rotacionar vetores para, que possamos |
+//| ver melhor o Objeto que temos                     |
+//|___________________________________________________|
+}
+
 function transa_Z({x, y, z}, dz){
     return {x, y, z: z + dz};
 }
 
 function quadros(){
-    dz += 1*dt;
+    //dz += 1*dt; (serve para distanciar o Objeto)
+    angulo += Math.PI*dt;
     limpar();
     for (const v of vs){
-    pont(tela(progecao(transa_Z(v, dz))));
-}
+    pont(tela(progecao(transa_Z(rotacao_xz(v, angulo), dz))));
+    }
+    for (const f of fs){
+        for (let i = 0; i < f.length; ++1){
+            const a = vs[f[i]];
+            const b = vs[f[(i+1)%f.length]];
+            linha (
+                tela(progecao(transa_Z(rotacao_xz(a, angulo), dz)))),
+                tela(progecao(transa_Z(rotacao_xz(b, angulo), dz)))),
+            )
+        }
+    // _________________________________________________________________
+    //| com isso ele vai pegar o array e fazer uma caminho entre eles   |
+    //| começando no ( 0 > 1 | 1 > 2 ) e por ai vai ate chegar no final |
+    //|_________________________________________________________________|
+    }
     setTimeout(quadros, 1000/FPS);
     // __________________________________________________________
     //| usando (DZ) ele esta fazendo uma pequena animação em     |
